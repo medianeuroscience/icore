b38ea1c3ae4cc660d8c62a873d469dafb8d5b226
musainayatmalik <mim290@nyu.edu>
2020-02-06 19:26:56 -0800

    present code can sift through themes and issues

--- flask_server/pyspark_testFile.py
+++ flask_server/pyspark_testFile.py
@@ -2,13 +2,14 @@ from flask import Flask, request, jsonify   #backend server to communicate data
 from flask_cors import CORS, cross_origin   #allow cross webpage resource sharing
 
 
+
 import os
 import sys
 import time
 
 import pandas as pd
 import numpy as np
-import pyspark.sql.functions as f
+from pyspark.sql.functions import explode
 
 #import findspark
 #findspark.init()
@@ -30,6 +31,27 @@ sqlContext = SQLContext(sc)
 app = Flask(__name__)
 
 CORS(app, resources={r"/api/*": {"origins": "*"}})
+
+issues_dict = {"taxes" : ["ECON_TAXATION",],
+"unemployment" : [ "UNEMPLOYMENT", ],
+"domestic economy" : ["ECON_BANKRUPTCY", "ECON_BOYCOTT", "ECON_COST_OF_LIVING", "ECON_CUTOUTLOOK", "ECON_DEREGULATION", "ECON_EARNINGSREPORT", "ECON_ENTREPRENEURSHIP", "ECON_HOUSING_PRICES", "ECON_INFORMAL_ECONOMY", "ECON_IPO", "ECON_INTEREST_RATE", "ECON_MONOPOLY", "ECON_MOU", "ECON_NATIONALIZE", "ECON_PRICECONTROL", "ECON_REMITTANCE", "ECON_STOCKMARKET", "ECON_SUBSIDIES", "ECON_UNIONS", "SLFID_ECONOMIC_DEVELOPMENT", "SLFID_ECONOMIC_POWER", "SOC_ECONCOOP"],
+"trade" : ["ECON_TRADE_DISPUTE", "ECON_FOREIGNINVEST", "ECON_FREETRADE", "ECON_CURRENCY_EXCHANGE_RATE", "ECON_CURRENCY_RESERVES", "ECON_DEBT"],
+"terrorism" : ["TAX_TERROR_GROUP", "SUICIDE_ATTACK", "EXTREMISM", "JIHAD", "TERROR", "WMD"],
+"military" : ["ACT_FORCEPOSTURE", "ARMEDCONFLICT", "BLOCKADE", "CEASEFIRE", "MILITARY", "MILITARY_COOPERATION", "PEACEKEEPING", "RELEASE_HOSTAGE", "SEIGE", "SLFID_MILITARY_BUILDUP", "SLFID_MILITARY_READINESS", "SLFID_MILITARY_SPENDING", "SLFID_PEACE_BUILDING", "TAX_MILITARY_TITLE"],
+"international relations" : ["GOV_INTERGOVERNMENTAL", "SOC_DIPLOMCOOP", "RELATIONS"],
+"immigration/refugees" : ["BORDER", "CHECKPOINT", "DISPLACED",  "EXILE", "IMMIGRATION", "REFUGEES", "SOC_FORCEDRELOCATION", "SOC_MASSMIGRATION", "UNREST_CHECKPOINT", "UNREST_CLOSINGBORDER"],
+"health care" : ["GENERAL_HEALTH", "HEALTH_SEXTRANSDISEASE", "HEALTH_VACCINATION", "MEDICAL", "MEDICAL_SECURITY"],
+"gun control" : ["FIREARM_OWNERSHIP", "MIL_SELF_IDENTIFIED_ARMS_DEAL", "MIL_WEAPONS_PROLIFERATION"],
+"drug" : ["CRIME_ILLEGAL_DRUGS", "DRUG_TRADE", "TAX_CARTELS", "CRIME_CARTELS"],
+"police system" : ["UNREST_POLICEBRUTALITY", "SECURITY_SERVICES"],
+"racism" : ["DISCRIMINATION", "HATE_SPEECH"],
+"civil liberties" : ["GENDER_VIOLENCE", "LGBT", "MOVEMENT_SOCIAL",  "MOVEMENT_WOMENS", "SLFID_CIVIL_LIBERTIES"],
+"environment" :  ["ENV_BIOFUEL", "ENV_CARBONCAPTURE", "ENV_CLIMATECHANGE",  "ENV_COAL", "ENV_DEFORESTATION", "ENV_FISHERY", "ENV_FORESTRY", "ENV_GEOTHERMAL", "ENV_GREEN", "ENV_HYDRO", "ENV_METALS", "ENV_MINING", "ENV_NATURALGAS", "ENV_NUCLEARPOWER", "ENV_OIL", "ENV_OVERFISH", "ENV_POACHING", "ENV_WATERWAYS ", "ENV_SOLAR", "ENV_SPECIESENDANGERED", "ENV_SPECIESEXTINCT", "ENV_WINDPOWER", "FUELPRICES", "MOVEMENT_ENVIRONMENTAL", "SELF_IDENTIFIED_ENVIRON_DISASTER", "SLFID_MINERAL_RESOURCES", "SLFID_NATURAL_RESOURCES", "WATER_SECURITY"],
+"party-politics" : ["TAX_POLITICAL_PARTY"],
+"election fraud" : ["ELECTION_FRAUD"],
+"education" : ["EDUCATION"],
+"media/internet" : ["CYBER_ATTACK",  "INTERNET_BLACKOUT", "INTERNET_CENSORSHIP", "MEDIA_CENSORSHIP", "MEDIA_MSM", "MEDIA_SOCIAL", "SURVEILLANCE", "FREESPEECH"],
+}
 
 #initiate flask app
 
@@ -92,7 +114,7 @@ def userGkgP():
         month.append(data['month'])
 
     if data['topic'] == 'default':
-        data['topic'] = 'empty'
+        data['topic'] = 'themes'
         topic.append(data['topic'])
     else:
         topic.append(data['topic'])
@@ -110,7 +132,7 @@ def userGkgP():
         user_email.append(data['email'])
 
     if data['country'] == 'default':
-        data['country'] = 'empty'
+        data['country'] = 'source_location'
         country.append(data['country'])
     else:
         country.append(data['country'])
@@ -122,7 +144,7 @@ def userGkgP():
         keyword.append(data['keyword'])
 
     if data['issue'] == 'default':
-        data['issue'] = 'empty'
+        data['issue'] = 'issue'
         issue.append(data['issue'])
     else:
         issue.append(data['issue'])
@@ -160,9 +182,14 @@ def userGkgG():
 
     time.sleep(1)
 
-    parameters = {'gkg_day': str(year[0]), 'source_location': str(country[0]), 'source': str(entity[0]), 'gcam_data': str(dictionary[0])}
+
+    parameters = {'gkg_day': str(year[0]), 'source_location': str(country[0]), 'named_entities': str(entity[0]), 'gcam_data': str(dictionary[0]), 'themes': str(topic[0]).upper(),
+                  'source': 'source', 'gkg_id': 'gkg_id'}
 
     filtered_param = {k: v for (k, v) in parameters.items() if v != 'empty'}
+
+    if str(issue[0]) != 'issue':
+        themes_issue = issues_dict[str(issue[0]).lower()]
 
     paras = ','.join("{}".format(k) for k, v in filtered_param.items() if v)
 
@@ -177,14 +204,15 @@ def userGkgG():
     cassDf = sqlContext.read.format("org.apache.spark.sql.cassandra")\
     .options(table= "gkg_record_by_day", keyspace = "icore_new")\
     .load()
-   #.select('gkg_day', 'gcam', 'location', 'tone_avg', 'format')
 
-
-    _start = str(year[0]) + '/'+ str(month[0]) + '/01'
-    _end =  str(year[0]) + '/'+ str(month[0]) + '/03'
+    if len(month[0]) > 1:
+        _start = str(year[0]) + '/'+ str(month[0]) + '/01'
+        _end =  str(year[0]) + '/'+ str(month[0]) + '/30'
 
     time_range = pd.date_range(start=_start, end=_end)
     time_range= time_range.values.astype('<M8[D]').astype(str)
+
+    #cassDf = cassDf.repartition(10)
 
 
     sqlDf = cassDf.registerTempTable('sqlTable')
@@ -192,19 +220,39 @@ def userGkgG():
 
     sqlDfList = []
 
+    start_time = time.time()
     for t in time_range:
         cassDF_byTime = sqlContext.sql("""SELECT {} FROM sqlTable WHERE gkg_day = '{}'""".format(paras, t))
-        cassDF_byTime = cassDF_byTime.filter(cassDF_byTime.source_location == str(country[0]))
-        #cassDF_byTime.toPandas().to_csv(str(i)+'_files.csv')
 
-        cass_Pandas = cassDF_byTime.toPandas()
+        if str(country[0]) != 'source_location':
+            cassDF_byTime = cassDF_byTime.filter(cassDF_byTime.source_location == str(country[0]))
+
+        if str(topic[0]) != 'themes':
+            df2 = cassDF_byTime.select(cassDF_byTime.gkg_id, explode(cassDF_byTime.themes).alias("themes2"))
+            df2 = df2.filter(df2.themes2 == str(topic[0]).upper())
+            df2 = df2.join(cassDF_byTime, on=['gkg_id'], how='inner').drop(df2.themes2)
+
+        if str(issue[0]) != 'issue':
+            df2 = cassDF_byTime.select(cassDF_byTime.gkg_id, explode(cassDF_byTime.themes).alias("themes2"))
+            df2 = df2.filter(df2.themes2.isin(themes_issue))
+            df2 = df2.join(cassDF_byTime, on=['gkg_id'], how='inner').drop(df2.themes2)
+
+        cass_Pandas = df2.toPandas()
         sqlDfList.append(cass_Pandas)
 
     #query through a sql context
+
+    print(" %s " % (time.time() - start_time))
+
     sqlDfList_output = pd.concat(sqlDfList)
     sqlDfList_output.to_csv('output_iter_files2.csv')
 
-    print(sqlDfList_output.head(5), sqlDfList_output(5))
+    print(len(sqlDfList_output))
+    print(sqlDfList_output.tail(5))
+    print(themes_issue)
+
+
+    #print(cassDF_byTime.rdd.getNumPartitions())
 
 
     #words = [year, entity, month, topic, dictionary, user_email, country, keyword, issue, meta, format]
